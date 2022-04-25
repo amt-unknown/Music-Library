e2358ccee15a67b92b3723ae55fa5d9fc5e69b58
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-04-25 15:51:56 -0700

    Album and Artist views added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Gallery from './components/Gallery'
 import Searchbar from './components/Searchbar';
 import { DataContext } from './context/DataContext';
 import { SearchContext } from './context/SearchContext';
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView';
 import './App.css';
 
 function App() {
@@ -32,16 +35,27 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-        }}>
-        <Searchbar />
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <Fragment>
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+                }}>
+                <Searchbar />
+              </SearchContext.Provider>
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          }/>
+          <Route path='/album/:id' element={<AlbumView />} />
+          <Route path='artist/:id' element={<ArtistView />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
